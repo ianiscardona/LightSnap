4f49c75857b45b005cdf701a449d1b069050e31e
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-03-07 12:43:35 +0800

    initial directories + resources

---
+++ src/components/HomeBenefits.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HomeBenefits = () => {
+  return <div>HomeBenefits</div>;
+};
---
+++ src/components/HomeHero.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HomeHero = () => {
+  return <div>HomeHero</div>;
+};
