59738f7e7aae5471e06f53bbaa74f7dcdae56a5e
JOLenizo <jolenizo2019@plm.edu.ph>
2023-03-21 14:04:11 +0800

    dummy auth

--- src/lightsnap_app/components/EventCode.jsx
+++ src/lightsnap_app/components/EventCode.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from "react-router-dom"
 
 export const EventCode = () => {
+
+  const [code, isCode] = useState("");
+  let auth = false;
+  const pass = "123";
+  if(code === pass){
+    auth = true;
+  }else{
+    auth = false;
+  }
+
   return (
     <div className='flex flex-col relative items-center w-full h-screen overflow-hidden mx-auto'>
         <div className='relative'>
@@ -27,10 +37,14 @@ export const EventCode = () => {
                 <p className="text-center text-base font-medium w-80 px-[.5rem] pt-5 pb-4 md:px-0">A photobooth that you can access anywhere, with frames tailored for your event.</p>
                 
                 <form className='flex flex-col justify-center text-center items-center gap-y-2'>
-                    <input type="text" placeholder="Enter Event Code" className=' border-[1px] border-[#000000] text-[#000000] text-center rounded-xl w-80 h-12 sm:w-96 sm:h-16'/>
+                    <input onChange={(e) => isCode(e.target.value)} type="text" placeholder="Enter Event Code" className=' border-[1px] border-[#000000] text-[#000000] text-center rounded-xl w-80 h-12 sm:w-96 sm:h-16'/>
+                    {auth ? 
                     <Link to='/getstarted'>
                       <input type="submit" value='Enter' className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"/>               
+                    </Link>:<Link to='/eventcodeerror'>
+                      <input type="submit" value='Enter' className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"/>               
                     </Link>
+                    }
                 </form>
                 
                 <Link to="/contact"><button className=' underline text-[#1C0EB7] font-medium text-sm sm:text-xl '>Contact Us</button></Link>
