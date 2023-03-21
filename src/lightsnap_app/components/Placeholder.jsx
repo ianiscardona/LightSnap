9195f3c4731122998a5efdfb46aea2cb28e8dde1
JOLenizo <jolenizo2019@plm.edu.ph>
2023-03-21 12:33:08 +0800

    edited placeholder

--- src/lightsnap_app/components/Placeholder.jsx
+++ src/lightsnap_app/components/Placeholder.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { AfterCamModal } from './AfterCamModal'
+import { GalleryModal } from './GalleryModal'
 
 export const Placeholder = () => {
   const [openModal, setOpenModal] = useState(false)
@@ -7,7 +7,7 @@ export const Placeholder = () => {
     <div className='w-full h-full'>
       <button onClick={() => setOpenModal(true)} className=' p-3 w-full h-full '></button>
       {/* <p className='text-4xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, maxime asperiores commodi porro amet illum blanditiis aliquam temporibus pariatur, expedita, excepturi voluptas deleniti quod ducimus! Earum porro repudiandae odit tenetur.</p> */}
-      <AfterCamModal open={openModal} onClose={() => setOpenModal(!openModal)} />
+      <GalleryModal open={openModal} onClose={() => setOpenModal(!openModal)} />
     </div>
   )
 }
