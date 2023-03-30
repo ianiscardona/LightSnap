38de4572353f2f6bd3abb796a4741985bff49d1f
Battosai101 <cyrruzzc@gmail.com>
2023-03-30 13:44:33 +0800

    Icons + title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Home } from "./pages/home";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { EventCode } from "./lightsnap_app/components/EventCode";
 import { EventCodeError } from "./lightsnap_app/components/EventCodeError";
 import { GetStartedCard } from "./lightsnap_app/components/GetStartedCard";
@@ -16,11 +16,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/eventgallery" element={<EventGallery />} />
         <Route path="/eventcode" element={<EventCode />} />
-        <Route path="/eventcodeerror" element={<EventCodeError/>} />
+        <Route path="/eventcodeerror" element={<EventCodeError />} />
         <Route path="/getstarted" element={<GetStartedCard />} />
         <Route path="/walkthrough" element={<WalkthroughCard />} />
         <Route path="/photobooth" element={<AppDashboard />} />
-        <Route path ="/contact" element={<HomeContact/>}/>
+        <Route path="/contact" element={<HomeContact />} />
       </Routes>
     </>
   );
