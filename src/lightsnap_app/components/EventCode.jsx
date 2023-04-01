fad2a0bac22738762133ace400bf6f934196524f
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-04-01 23:23:32 +0800

    Update EventCode.jsx

--- src/lightsnap_app/components/EventCode.jsx
+++ src/lightsnap_app/components/EventCode.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { setCookie } from "nookies";
 
 //import axios from 'axios'
 //import React,{useContext, useState} from 'react'
@@ -24,14 +25,29 @@ import { Link } from "react-router-dom";
       } */
 
 export const EventCode = () => {
-  const [code, isCode] = useState("");
-  let auth = false;
+//   const [code, isCode] = useState("");
+//   let auth = false;
+//   const pass = "bday";
+//   if (code === pass) {
+//     auth = true;
+//   } else {
+//     auth = false;
+//   }
+  const [code, setCode] = useState("");
   const pass = "bday";
-  if (code === pass) {
-    auth = true;
-  } else {
-    auth = false;
-  }
+
+  const handleCodeSubmit = (e) => {
+    e.preventDefault();
+    if (code === pass) {
+      setCookie(null, "isAuthenticated", "true", {
+        maxAge: 30 * 24 * 60 * 60,
+        path: "/",
+      });
+      window.location.href = "/getstarted";
+    } else {
+      window.location.href = "/eventcodeerror";
+    }
+  };
 
   return (
     <div className="flex flex-col relative items-center w-full h-screen overflow-hidden mx-auto">
@@ -84,32 +100,49 @@ export const EventCode = () => {
           A photobooth that you can access anywhere, with frames tailored for
           your event.
         </p>
-
-        <form className="flex flex-col justify-center text-center items-center gap-y-2">
+        <form
+          onSubmit={handleCodeSubmit}
+          className="flex flex-col justify-center text-center items-center gap-y-2"
+        >
           <input
-            onChange={(e) => isCode(e.target.value)}
+            onChange={(e) => setCode(e.target.value)}
             type="text"
             placeholder="Enter Event Code"
-            className=" border-[1px] border-[#000000] text-[#000000] text-center rounded-xl w-80 h-12 sm:w-96 sm:h-16"
+            className="border-[1px] border-[#000000] text-[#000000] text-center rounded-xl w-80 h-12 sm:w-96 sm:h-16"
           />
-          {auth ? (
-            <Link to="/getstarted">
-              <input
-                type="submit"
-                value="Enter"
-                className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"
-              />
-            </Link>
-          ) : (
-            <Link to="/eventcodeerror">
-              <input
-                type="submit"
-                value="Enter"
-                className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"
-              />
-            </Link>
-          )}
+          <button
+            type="submit"
+            className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"
+          >
+            Enter
+          </button>
         </form>
+
+//         <form className="flex flex-col justify-center text-center items-center gap-y-2">
+//           <input
+//             onChange={(e) => isCode(e.target.value)}
+//             type="text"
+//             placeholder="Enter Event Code"
+//             className=" border-[1px] border-[#000000] text-[#000000] text-center rounded-xl w-80 h-12 sm:w-96 sm:h-16"
+//           />
+//           {auth ? (
+//             <Link to="/getstarted">
+//               <input
+//                 type="submit"
+//                 value="Enter"
+//                 className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"
+//               />
+//             </Link>
+//           ) : (
+//             <Link to="/eventcodeerror">
+//               <input
+//                 type="submit"
+//                 value="Enter"
+//                 className="flex items-center justify-center text-center rounded-full w-28 h-10 sm:w-52 sm:h-16 text-white bg-[#1C0EB7] hover:bg-[#D7282F] focus:bg-[#D7282F] transition-colors duration-300"
+//               />
+//             </Link>
+//           )}
+//         </form>
 
         <Link to="/contact">
           <button className=" underline text-[#1C0EB7] font-medium text-sm sm:text-xl ">
